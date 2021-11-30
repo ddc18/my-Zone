6993b2601d803d92492ddd9c959d76f3d8448c35
v_mdpeng <v_mdpeng@tencent.com>
2021-11-30 16:40:29 +0800

    --test

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+require('../css/app.css');
+require('../css/reset.css');
+
+console.log('hello');
---
+++ webpack.config.js
@@ -0,0 +1,115 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCss = require('optimize-css-assets-webpack-plugin');
+// const uglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  devServer: {
+    port: 3000,
+    open: true,
+    contentBase: './dist',
+    hot: true
+  },
+  entry: {
+    index: './src/js/app.js'
+  },
+  output: {
+    filename: 'js/[name].[hash:8].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  //devtool 不会产生单独的文件， 但是可以显示行和列
+  devtool: 'eval-source-map',
+  // optimization: { // 优化项
+  //   minimizer: [
+  //     new uglifyjsWebpackPlugin(),          //压缩js 用了OptimizeCss需设置js压缩
+  //     new OptimizeCss()                     //压缩css
+  //   ]
+  // },
+  plugins: [
+    // 清除dist文件夹
+    new CleanWebpackPlugin(),
+    // 压缩css
+    new OptimizeCss({
+      cssProcessor: require('cssnano'), //引入cssnano配置压缩选项
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
+      canPrint: true //是否将插件信息打印到控制台
+    }),
+    // html中自动添加 script
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      // filename: 'view/index.html',   // 使用link引入css时 img路径问题需跟css url一致
+      filename: 'index.html',
+      chunks: ['index'],
+      hash: true
+    }),
+    // 将样式通过link标签引入
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[hash:8].css'
+    }),
+    new webpack.NamedChunksPlugin(),        // 打印更新的模块路劲
+    new webpack.HotModuleReplacementPlugin()// 热更新插件
+  ],
+  module: {
+    rules: [
+      // html中引入的图片进行打包
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+              attr: ['img:src', 'img:data-src']
+            }
+          }
+        ]
+      },
+
+      //css js中引入图片进行打包
+      {
+        test: /\.(png|jpg|gif)$/,
+        // 做一个限制 当我们的图片小于多少K的时候 用base64来转化
+        // 否则用file-loader 
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 1,    // 小于200k
+            outputPath: 'images/',
+            publicPath: '/images/'
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-proposal-class-properties',    // 将class语法转化
+            ],
+          }
+        },
+        include: path.resolve(__dirname, 'src'),  // 希望找这个文件下的js
+        // exclude: /node_modules/ // 排除这个文件下的js
+      },
+      // style-loader 把css插入到head的标签中
+      // MiniCssExtractPlugin 将样式通过link标签引入
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          // 'style-loader',
+          'css-loader',
+          'postcss-loader',   //自动加css前缀 需要配置文件postcss.config.js
+        ]
+      }
+    ]
+  }
+}
